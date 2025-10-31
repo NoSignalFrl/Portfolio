a480b8f4bb70d383600f7bdc6f004025d0d07a27
Toni <toni11elia11@gmail.com>
2025-10-31 04:49:28 -0400

    [Add] Ajout du code des méthodes CRUD du UserController
    Il y a un warning qui vient de VSCode, non pas de PHP, qui dit que languages est protégé et ne peut pas être accessible. Apparemment, c'est normal...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,83 +2,140 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * Affiche la liste de tous les utilisateurs
      *
-     * @return \Illuminate\Http\Response
+     * Ici, on charge tous les users, et en même temps, on charge leurs skills et expériences associées. C'est comme un chargement anticipé
+     * C'est pour charger les relations d'un coup
+     * @return \Illuminate\View\View
      */
     public function index()
     {
-        //
+        $users = User::with(['skills', 'experiences'])->get();
+        return view('users.index', compact('users'));
     }
+
 
     /**
      * Show the form for creating a new resource.
+     * Affiche le formulaire pour créer un nouvel utilisateur.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        //
+        return view('users.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Enregistre un nouvel utilisateur dans la base de données.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'name' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:255',
+            'postal_code' => 'nullable|string|max:255',
+            'languages' => 'nullable|string|max:255',
+            'birthday' => 'nullable|date',
+        ]);
+
+        // Créer le user
+        $user = new User([
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'postal_code' => $request->postal_code,
+            'languages' => $request->languages,
+            'birthday' => $request->birthday,
+        ]);
+
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'Utilisateur ajouté avec succès.');
     }
 
     /**
-     * Display the specified resource.
+     * Affiche un utilisateur spécifique et ses relations.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function show($id)
     {
-        //
+        $user = User::with(['skills', 'experiences'])->findOrFail($id);
+        return view('users.show', compact('user'));
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Affiche le formulaire pour modifier un utilisateur.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id);
+        return view('users.edit', compact('user'));
     }
 
     /**
-     * Update the specified resource in storage.
+     * Met à jour un utilisateur existant.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'email' => 'required|email|unique:users,email,' . $id,
+            'password' => 'nullable|min:6',
+            'name' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:255',
+            'postal_code' => 'nullable|string|max:255',
+            'languages' => 'nullable|string|max:255',
+            'birthday' => 'nullable|date',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $data = $request->all();
+
+        if ($request->filled('password')) {
+            $data['password'] = bcrypt($request->password);
+        } else {
+            unset($data['password']); // évite d'écraser le mdp si vide
+        }
+
+        $user->update($data);
+
+        return redirect()->route('users.index')->with('success', 'Utilisateur mis à jour avec succès.');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Supprime un utilisateur de la base de données.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return redirect()->route('users.index')->with('success', 'Utilisateur supprimé avec succès.');
     }
 }
