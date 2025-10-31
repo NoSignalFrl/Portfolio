8328f2eaf862da58b36acae21c441c34aab4c946
Toni <toni11elia11@gmail.com>
2025-10-31 01:19:17 -0400

    [Add] Ajout des relations dans le modèle Skill.php

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     use HasFactory;
+
+    /**
+     * Les attributs qu'on peut remplir en masse
+     */
+    protected $fillable = [
+        'skill',
+        'description',
+        'user_id',
+    ];
+
+    /**
+     * Relation : une compétence appartient à un utilisateur
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
